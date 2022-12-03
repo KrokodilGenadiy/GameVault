260bf65eb068e53eb1008ad6a94ccae7d93d31d5
KrokodilGenadiy <alex.usevich@gmail.com>
2022-12-03 20:37:10 +0300

    changed design a litlle bit

--- app/src/main/java/com/zaus_app/gamevault/MainActivity.kt
+++ app/src/main/java/com/zaus_app/gamevault/MainActivity.kt
@@ -35,24 +35,28 @@ class MainActivity : AppCompatActivity() {
                     val tag = "home"
                     val fragment = checkFragmentExistence(tag)
                     window.statusBarColor = ContextCompat.getColor(this,R.color.colorFragmentHome)
+                    binding.bottomNavigation.setBackgroundResource(R.drawable.home_fragment_background)
                     changeFragment( fragment?: HomeFragment(), tag)
                 }
                 R.id.favorites -> {
                     val tag = "favorites"
                     val fragment = checkFragmentExistence(tag)
                     window.statusBarColor = ContextCompat.getColor(this,R.color.colorFragmentFavorites)
+                    binding.bottomNavigation.setBackgroundResource(R.drawable.favorites_fragment_background)
                     changeFragment( fragment?: FavoritesFragment(), tag)
                 }
                 R.id.news -> {
                     val tag = "news"
                     val fragment = checkFragmentExistence(tag)
                     window.statusBarColor = ContextCompat.getColor(this,R.color.colorFragmentNews)
+                    binding.bottomNavigation.setBackgroundResource(R.drawable.news_fragment_background)
                     changeFragment( fragment?: NewsFragment(), tag)
                 }
                 R.id.settings -> {
                     val tag = "settings"
                     val fragment = checkFragmentExistence(tag)
                     window.statusBarColor = ContextCompat.getColor(this,R.color.colorFragmentSettings)
+                    binding.bottomNavigation.setBackgroundResource(R.drawable.settings_fragment_background)
                     changeFragment( fragment?: SettingsFragment(), tag)
                 }
             }
